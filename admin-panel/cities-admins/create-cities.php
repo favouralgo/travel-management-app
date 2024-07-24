23c77b4cf5b83283b71ef46102967946ab5e3b5c
Favour M <favourmadubuko1@gmail.com>
2024-07-24 23:25:34 +0000

    chore: Update admin panel layout and fix file paths

--- admin-panel/cities-admins/create-cities.php
+++ admin-panel/cities-admins/create-cities.php
@@ -1,67 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
-    <title>Admin Panel</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-     <link href="../styles/style.css" rel="stylesheet">
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div id="wrapper">
-    <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
-      <div class="container">
-      <a class="navbar-brand" href="#">LOGO</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<?php require "../layout/header.php"; ?>      
+<?php require "../../config/connection.php"; ?>
 
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav side-nav" >
-          <li class="nav-item">
-            <a class="nav-link" style="margin-left: 20px;" href="../index.php">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../admins/admins.php" style="margin-left: 20px;">Admins</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../countries-admins/show-country.php" style="margin-left: 20px;">Countries</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../cities-admins/show-cities.php" style="margin-left: 20px;">Cities</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../bookings-admins/show-bookings.php" style="margin-left: 20px;">Bookings</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ml-md-auto d-md-flex">
-          <li class="nav-item">
-            <a class="nav-link" href="../index.php">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              username
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Logout</a>
-              
-          </li>
-                          
-          
-        </ul>
-      </div>
-    </div>
-    </nav>
-    <div class="container-fluid">
-       <div class="row">
+      <div class="row">
         <div class="col">
           <div class="card">
             <div class="card-body">
--- admin-panel/cities-admins/show-cities.php
+++ admin-panel/cities-admins/show-cities.php
@@ -1,68 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
-    <title>Admin Panel</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-     <link href="../styles/style.css" rel="stylesheet">
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div id="wrapper">
-    <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
-      <div class="container">
-      <a class="navbar-brand" href="#">LOGO</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<?php require "../layout/header.php"; ?>      
+<?php require "../../config/connection.php"; ?>
 
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav side-nav" >
-          <li class="nav-item">
-            <a class="nav-link" style="margin-left: 20px;" href="../index.php">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../admins/admins.php" style="margin-left: 20px;">Admins</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../regions-admins/show-country.php" style="margin-left: 20px;">Regions</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../cities-admins/show-cities.php" style="margin-left: 20px;">Cities</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../bookings-admins/show-bookings.php" style="margin-left: 20px;">Bookings</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ml-md-auto d-md-flex">
-          <li class="nav-item">
-            <a class="nav-link" href="../index.php">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              username
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Logout</a>
-              
-          </li>
-                          
-          
-        </ul>
-      </div>
-    </div>
-    </nav>
-    <div class="container-fluid">
+<?php
 
-          <div class="row">
+if (!isset($_SESSION["adminname"])) {
+  header("location: " . ADMINURL);
+  exit();
+}
+
+// QUERY TO
+// Prepare and execute the query
+$cities = $connection->prepare("SELECT * FROM cities");
+$cities->execute();
+
+// Fetch results
+$cityResult = $cities->get_result();
+$allCities = $cityResult->fetch_all(MYSQLI_ASSOC);
+
+
+// Prepare and execute the query
+$region = $connection->prepare("SELECT * FROM regions");
+$region->execute();
+
+// Fetch results
+$regionResult = $region->get_result();
+$allRegions = $regionResult->fetch_all(MYSQLI_ASSOC);
+
+?>
+      <div class="row-2">
         <div class="col">
           <div class="card">
             <div class="card-body">
@@ -72,31 +37,15 @@
               <table class="table">
                 <thead>
                   <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">name</th>
+                    <th scope="col">City ID</th>
+                    <th scope="col">City Name</th>
                     <th scope="col">image</th>
-                    <th scope="col">trip_days</th>
-                    <th scope="col">price</th>
-                    <th scope="col">delete</th>
+                    <th scope="col">Trip Days</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Region ID</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Berlin</td>
-                    <td>image</td>
-                    <td>3</td>
-                    <td>$1300</td>
-                     <td><a href="delete-posts.html" class="btn btn-danger  text-center ">delete</a></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Berlin</td>
-                    <td>image</td>
-                    <td>3</td>
-                    <td>$1300</td>
-                     <td><a href="delete-posts.html" class="btn btn-danger  text-center ">delete</a></td>
-                  </tr>
                   <tr>
                     <th scope="row">1</th>
                     <td>Berlin</td>
@@ -111,12 +60,4 @@
           </div>
         </div>
       </div>
-
-
-
-  </div>
-<script type="text/javascript">
-
-</script>
-</body>
-</html>
+<?php require "../layout/footer.php"; ?>
