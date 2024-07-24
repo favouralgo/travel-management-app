f739aa06fd1adb72aaeb3c193ca5784e379d0a64
Favour M <favourmadubuko1@gmail.com>
2024-07-24 03:51:43 +0000

    chore: Update admin panel layout and fix file paths

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -1,42 +1,67 @@
 <?php require "layout/header.php"; ?>
+<?php require '../config/connection.php';?>
 
-<?php if(!isset($_SESSION["adminname"])){
+<?php
+if (!isset($_SESSION["adminname"])) {
     header("Location: ".ADMINURL."admins/login-admins.php");
+    exit();
 }
-;?>
-    <div class="container-fluid">
-            
-      <div class="row">
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Countries</h5>
-              <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-              <p class="card-text">Number of countries: 8</p>
-             
+// Query for regions
+$regionsQuery = "SELECT COUNT(*) AS region_count FROM regions";
+$regionsResult = $connection->query($regionsQuery);
+$allRegions = $regionsResult->fetch_object();
+
+// Query for cities
+$citiesQuery = "SELECT COUNT(*) AS city_count FROM cities";
+$citiesResult = $connection->query($citiesQuery);
+$allCities = $citiesResult->fetch_object();
+
+// Query for admins
+$adminsQuery = "SELECT COUNT(*) AS admin_count FROM admins";
+$adminsResult = $connection->query($adminsQuery);
+$allAdmins = $adminsResult->fetch_object();
+
+// Query for bookings
+$bookingsQuery = "SELECT COUNT(*) AS booking_count FROM bookings";
+$bookingsResult = $connection->query($bookingsQuery);
+$allBookings = $bookingsResult->fetch_object();
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Regions</h5>
+                    <p class="card-text">No. of Regions: <?php echo htmlspecialchars($allRegions->region_count); ?></p>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Cities</h5>
-              
-              <p class="card-text">Number of cities: 4</p>
-              
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Cities</h5>
+                    <p class="card-text">No. of cities: <?php echo htmlspecialchars($allCities->city_count); ?></p>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Admins</h5>
-              
-              <p class="card-text">Number of admins: 3</p>
-              
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Admins</h5>
+                    <p class="card-text">No. of Admins: <?php echo htmlspecialchars($allAdmins->admin_count); ?></p>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-     
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Bookings</h5>
+                    <p class="card-text">No. of bookings: <?php echo htmlspecialchars($allBookings->booking_count); ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php require "layout/footer.php"; ?>
