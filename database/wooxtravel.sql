10be5b779a6db0965d3d6df4967ec8c31009e189
Favour M <favourmadubuko1@gmail.com>
2024-08-01 20:27:50 +0000

    codebase cleanup

--- database/wooxtravel.sql
+++ database/wooxtravel.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 31, 2024 at 01:30 PM
+-- Host: localhost
+-- Generation Time: Aug 01, 2024 at 10:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,12 +41,8 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `adminname`, `email`, `mypassword`, `created_at`, `role`) VALUES
-(3, 'Madu', 'favourmdev@gmail.com', '$2y$10$gDjNzKFsgBFNRS5qcU/rhuqtlzLxevE2xlJAD86Euc5g4.Z56Sp8a', '2024-07-24 00:20:17', 1),
-(4, 'Quality', 'quality@gmail.com', '$2y$10$/tpCO6joSG5QfczxUpQl9e92w5cRq6vs6JUYNxw5LH474.H1gkhTO', '2024-07-24 05:19:22', 2),
-(5, 'People', 'people@gmail.com', '$2y$10$0qXPUO0LRSsW2znYK5FrAODk.lpZaHmSMKB9bxkGkMCCJxVMy.Enq', '2024-07-24 18:47:48', 2),
-(6, 'Batman', 'testadmin@admin.com', '$2y$10$CwYsdrlF3BlSKwbTTqxjku7neL2sho4xqSv5QGjQYkQ91N.QQ9swW', '2024-07-25 08:40:26', 2),
-(7, 'Emmanuel', 'emma@gmail.com', '$2y$10$iKVJhywsad4OAH2g2cxr3e2uQ6pDXWngQJOvDQKysekxBDQJqlTz2', '2024-07-30 14:42:34', 2),
-(9, 'Neon', 'neon@gmail.com', '$2y$10$MewgyuiN5/8i8FbeL4fxx..9buGxz5oWmBUidBXnY6clpps1SQiba', '2024-07-31 09:10:27', 1);
+(1, 'Madu', 'favourmdev@gmail.com', '$2y$10$8sI1SS/iw1XawbJ4ZIUYlu3IpkUol.BhuqyO3dVEWwB7rMzE1VaQ6', '2024-08-01 15:40:49', 1),
+(2, 'Agyei', 'emma@gmail.com', '$2y$10$31xhlD9rR5mP9xw4G9VhKOPzQpWqjrmjZjqy58246.AFqzy0ssj3K', '2024-08-01 15:43:17', 2);
 
 -- --------------------------------------------------------
 
@@ -73,14 +69,9 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`id`, `name`, `phone_number`, `num_of_guests`, `checkin_date`, `destination`, `status`, `city_id`, `user_id`, `payment`, `created_at`) VALUES
-(1, 'Ruto M', 549722299, 3, '0000-00-00', 'Konkonuru', 'Pending', 3, 1, '', '2024-06-26 18:05:50'),
-(2, 'Malema', 547890123, 2, '0000-00-00', 'Cantonment', 'Pending', 4, 1, '', '2024-06-26 18:05:50'),
-(3, 'Amin', 54671221, 3, '0000-00-00', 'Konkonuru', 'Pending', 3, 1, '', '2024-06-26 19:11:46'),
-(15, 'No', 203456789, 3, '2024-06-28', 'Cantonment', 'Pending', 4, 4, '100', '2024-06-27 19:06:06'),
-(16, 'Utali', 203456589, 2, '2024-07-20', 'Cantonment', 'Booked Successfully', 4, 5, '1000', '2024-07-03 20:56:10'),
-(18, 'Bube', 203456589, 3, '2024-07-17', 'Accra', 'Pending', 1, 7, '600', '2024-07-16 08:53:23'),
-(20, 'cool', 203456589, 2, '2024-07-31', 'Ketasco', 'Booked Successfully', 13, 10, '1000', '2024-07-30 03:03:09'),
-(21, 'dev', 203456789, 1, '2024-07-31', 'Ketasco', 'Booked Successfully', 13, 10, '500', '2024-07-30 12:49:13');
+(2, 'Dave', 203456789, 3, '2024-08-13', 'Legon', 'Booked Successfully', 4, 16, '900', '2024-08-01 16:10:55'),
+(3, 'Dave', 203456789, 2, '2024-08-15', 'Tarkwa', 'Booked Successfully', 5, 16, '400', '2024-08-01 16:11:32'),
+(4, 'Dave', 203456789, 3, '2024-08-04', 'Tarkwa', 'Booked Successfully', 5, 16, '600', '2024-08-01 20:13:03');
 
 -- --------------------------------------------------------
 
@@ -103,12 +94,10 @@ CREATE TABLE `cities` (
 --
 
 INSERT INTO `cities` (`id`, `name`, `image`, `trip_days`, `price`, `region_id`, `created_at`) VALUES
-(1, 'Accra', 'offers-02.jpg', 4, '200', 1, '2024-06-25 15:00:27'),
-(2, 'Kasoa', 'offers-02.jpg', 6, '450', 1, '2024-06-25 15:00:27'),
-(3, 'Konkonuru', 'offers-02.jpg', 4, '600', 2, '2024-06-26 14:10:30'),
-(4, 'Cantonment', 'deals-01.jpg', 5, '500', 2, '2024-06-26 14:10:30'),
-(6, 'Aburi', 'deals-01.jpg', 5, '600', 3, '2024-07-25 00:17:05'),
-(11, 'Prempeh', 'cities-03.jpg', 4, '2500', 4, '2024-07-29 18:21:25');
+(2, 'Tamale', 'city-02.jpg', 4, '300', 2, '2024-08-01 15:48:43'),
+(4, 'Legon', 'cities-04.jpg', 2, '300', 4, '2024-08-01 15:58:43'),
+(5, 'Tarkwa', 'deals-02.jpg', 3, '200', 2, '2024-08-01 15:59:55'),
+(6, 'Koforidua', 'deals-04.jpg', 5, '700', 3, '2024-08-01 16:01:31');
 
 -- --------------------------------------------------------
 
@@ -131,10 +120,10 @@ CREATE TABLE `regions` (
 --
 
 INSERT INTO `regions` (`id`, `name`, `image`, `population`, `landmark`, `description`, `created_at`) VALUES
-(1, 'Eastern', 'banner-01.jpg', '1000', 'Berekuso', 'A good place to rest and explore', '2024-06-25 14:57:18'),
-(2, 'Central', 'banner-02.jpg', '1500', 'Akosombo', 'A place to appreciate nature', '2024-06-25 14:57:18'),
-(3, 'Western', 'banner-02.jpg', '1500', 'Kumasi', 'A place known as the food basket of the nation', '2024-07-24 20:16:36'),
-(4, 'Upper', 'banner-02.jpg', '3500', 'Keta', 'Nice', '2024-07-29 20:43:23');
+(2, 'Western', 'banner-02.jpg', '3500', 'Tarkwa', 'A place for everyday living', '2024-08-01 15:47:08'),
+(3, 'Eastern', 'banner-03.jpg', '4000', 'Aburi', 'A central heartland of Accra, boasting of its mall and shopping centers', '2024-08-01 15:54:25'),
+(4, 'Northern', 'banner-04.jpg', '3500', 'Tamale', 'The food basket of the nation of Ghana. Get all real prices of food ready and fresh from the farm', '2024-08-01 15:55:39'),
+(5, 'Upper', 'cities-01.jpg', '2100', 'Aburi', 'Get the natural taste of Ghanaian dishes here', '2024-08-01 16:42:45');
 
 -- --------------------------------------------------------
 
@@ -155,11 +144,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `userpassword`, `date_created`) VALUES
-(5, 'huvoge@mailinator.com', 'gafib', '$2y$10$doY1bAvtPgrvGdps7yQNHuqWD8K1CMzozLN/pfDM04t.ma7HwGXyW', '2024-07-03 11:40:48'),
-(6, 'gm@gmail.com', 'Loki', '$2y$10$5W/EBg6Jo.b6VUphsZbcyOrxxuOahWD32MsuGLZ8GZ/wBhftF9AXS', '2024-07-13 14:18:42'),
-(7, 'amazingbrain01@gmail.com', 'Bube', '$2y$10$38HCBDLlzzj61fmtPG9UMOYaf9FwkWM8FcPnkZJ791rjsoFNlEKtO', '2024-07-16 08:51:45'),
-(8, 'super@admin.com', 'Super', '$2y$10$FRClooDewvkRwR8XV63g8ulYF6JnD6oSwA8ecPHJdnB3647vx33VG', '2024-07-18 18:10:10'),
-(10, 'fm@gmail.com', 'dev', '$2y$10$xAoPmX9c39vIt3OCcmMpAecsGrs1lxScliAA6HIkaSWY9svTnr0XW', '2024-07-24 20:24:25');
+(13, 'ammaannorbea@gmail.com', 'amma', '$2y$10$FmAJAbSCfgyJZPsBaIWEX.WN0XhZBYldnB.ANRK7mSn//pAAYp1Si', '2024-07-31 22:04:47'),
+(14, 'Labijachubby24@gmail.com', 'Faith ', '$2y$10$6x4LKqKyuV2JogrdxbrNs.SmdJQIhHCHib.cPX8dcB8I3IUB5Qkqq', '2024-08-01 12:08:28'),
+(16, 'sdave@gmail.com', 'Dave', '$2y$10$jY5n5roK0PKNR3msX4S79ewKRJYmDA2LDBHdRu6BWJgRJlGo1pMr2', '2024-08-01 16:07:54');
 
 --
 -- Indexes for dumped tables
@@ -175,7 +162,9 @@ ALTER TABLE `admins`
 -- Indexes for table `bookings`
 --
 ALTER TABLE `bookings`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user` (`user_id`),
+  ADD KEY `fk_city` (`city_id`);
 
 --
 -- Indexes for table `cities`
@@ -204,35 +193,42 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `regions`
 --
 ALTER TABLE `regions`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `fk_city` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `cities`
