a6c2f4b128061ad67540f65e58a174382cf8cda5
Favour M <favourmadubuko1@gmail.com>
2024-08-01 13:50:38 +0000

    chore: Update region icons layout and font size in index.php

--- index.php
+++ index.php
@@ -33,16 +33,16 @@ $regions = $result->fetch_all(MYSQLI_ASSOC);
                 <div class="col-lg-12">
                   <div class="more-info">
                     <div class="row">
-                      <div class="col-lg-3 col-sm-6 col-6 text-center">
+                      <div class="col-lg-3 col-sm-6 text-center">
         <!--Dynamically fetch data from database-->
-                        <i class="fa fa-user mb-2"></i>
+                        <i class="fa fa-user"></i>
                         <h4><span>Population:</span><br><?php echo $region['population']; ?></h4>
                       </div>
                       <div class="col-lg-3 col-sm-6 col-6 text-center">
                         <i class="fa fa-globe mb-2"></i>
                         <h4><span>Landmark:</span><br><?php echo $region['landmark']; ?></h4>
                       </div>
-                      <div class="col-sm-6 col-6 text-center">
+                      <div class="col-lg-3 col-sm-6 col-6 text-center">
                         <i class="fa fa-home mb-2"></i>
                         <h4><span>AVG Price:</span><br>GHC <?php echo $region['average_price']; ?></h4>
                       </div>
